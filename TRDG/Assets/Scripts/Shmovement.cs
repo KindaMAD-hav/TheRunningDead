107238533dcc864f67a82bb3ca0e5c3d2b491fc9
Madhav Gangneja <kindamad.hav@gmail.com>
2024-11-05 06:22:08 +0530

    movement update

--- TRDG/Assets/Scripts/Shmovement.cs
+++ TRDG/Assets/Scripts/Shmovement.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Shmovement : MonoBehaviour
 {
     public float playerSpeed = 6;
     public float lateralPlayerSpeed = 4;
 
+    private Vector2 movementInput;
+
+    // Called when the player moves
+    public void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+    }
 
     void Update()
     {
+        // Forward movement
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * lateralPlayerSpeed);
-        }
-        if (Input.GetKey(KeyCode.D)){
 
-            transform.Translate(Vector3.right * Time.deltaTime * lateralPlayerSpeed * -1);
+        // Lateral movement
+        if (movementInput.x < 0) // A key or left
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * lateralPlayerSpeed, Space.World);
+        }
+        if (movementInput.x > 0) // D key or right
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * lateralPlayerSpeed, Space.World);
         }
     }
 }
